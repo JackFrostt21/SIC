64f8a413b0eb8213e43d96b26e9584dc2e5f20db
JackFrostt21 <stepanychev2101@gmail.com>
2024-04-01 01:24:41 +0300

    delete

--- BOTSERV/botserv/telegram_bot/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import DirectoryServices, Applications, BuildingEriell, FloorEriell, BlockEriell, TelegramUser
-
-admin.site.register(TelegramUser)
-admin.site.register(DirectoryServices)
-admin.site.register(Applications)
-admin.site.register(BuildingEriell)
-admin.site.register(FloorEriell)
-admin.site.register(BlockEriell)
