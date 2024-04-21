2c7d29ad06b2691be9a3801173e0c9b0e2d6e304
JackFrostt21 <stepanychev2101@gmail.com>
2024-04-22 01:28:12 +0300

    del

--- educational_bot/app/bot/management/commands/bot_logic/handlers/callbackhandlers/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import settings_logics
-from . import educational_logic
-from . import testing_logic
--- educational_bot/app/educational_module/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from app.educational_module.views import CourseViewSet, TopicCourseViewSet
-
-router = routers.DefaultRouter()
-router.register(r"course", CourseViewSet)
-router.register(r"topic_course", TopicCourseViewSet)
-
-urlpatterns = router.urls
